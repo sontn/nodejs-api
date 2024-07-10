606cd31c74549f778fe10f82e547fbf0c7a6c69e
Son Tran <son.tranngoc@gmail.com>
2024-07-10 15:02:04 +0700

    Middleware, logging with morgan

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
 
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use('/api/v1/bootcamps', bootcamps);
 
