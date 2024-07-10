93828e6660eee52cb66b5517b8c77ca6a3a4b11e
Son Tran <son.tranngoc@gmail.com>
2024-07-10 14:00:37 +0700

    Install express, dotenv

--- server.js
+++ server.js
@@ -1,42 +1,12 @@
-const { error } = require('console');
-const http = require('http');
+const express = require('express');
+const dotenv = require('dotenv');
 
-const todos = [
-  { id: 1, text: 'Todo One' },
-  { id: 2, text: 'Todo Two' },
-  { id: 3, text: 'Todo Three' },
-];
+dotenv.config({ path: './config/config.env' });
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 404;
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('X-Powered-By', 'Node.js');
+const app = express();
 
-  res.writeHead(400, {
-    'Content-Type': 'application/json',
-    'X-Powered-By': 'Node.js',
-  });
+const PORT = process.env.PORT || 5000;
 
-  let body = [];
-
-  req
-    .on('data', (chunk) => {
-      body.push(chunk);
-    })
-    .on('end', () => {
-      body = Buffer.concat(body).toString();
-      console.log(body);
-    });
-
-  res.end(
-    JSON.stringify({
-      success: false,
-      data: null,
-      error: 'Please add email',
-    })
-  );
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
-
-const PORT = 5000;
-
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
