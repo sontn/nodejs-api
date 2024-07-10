1fedcc54d98619f7598bc01f08cd9616eaeb6020
Son Tran <son.tranngoc@gmail.com>
2024-07-10 15:27:23 +0700

    Working with mongoose

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const moogoose = require('mongoose');
+
+const connectDB = async () => {
+  const conn = await moogoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+
+  console.log(`MongoDB Connected: ${conn.connection.host}`);
+};
