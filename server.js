beaad8233d3f80a24cec9b0bce737884c09e10cc
Son Tran <son.tranngoc@gmail.com>
2024-07-11 10:51:30 +0700

    Error handler middleware

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 dotenv.config({ path: './config/.env' });
 const connectDB = require('./config/db');
 const colors = require('colors');
+const errorHandler = require('./middleware/error');
 
 connectDB();
 
@@ -17,6 +18,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use('/api/v1/bootcamps', bootcamps);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
