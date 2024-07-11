115e6583bc6a8c185c01661b1ecdc378354cc954
Son Tran <son.tranngoc@gmail.com>
2024-07-11 11:38:51 +0700

    Error mongoose handles validation errors

--- middleware/error.js
+++ middleware/error.js
@@ -5,11 +5,17 @@ const errorHandler = (err, req, res, next) => {
 
   error.message = err.message;
 
-  console.log(err.stack.red);
+  console.log(err);
 
   // MongoServerError with 11000 code
   if (err.code === 11000) {
     const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
     error = new ErrorResponse(message, 400);
   }
 
