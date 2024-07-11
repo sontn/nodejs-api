5042d2a38cadb451ba55f3be1c01e8ab57dc32c5
Son Tran <son.tranngoc@gmail.com>
2024-07-11 11:00:12 +0700

    Custom error response class

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const ErrorResponse = require('../utils/errorResponse');
+
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc:     Get all bootcamps
@@ -21,14 +23,15 @@ exports.getBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findById(req.params.id);
     if (!bootcamp) {
-      return res
-        .status(404)
-        .json({ success: false, error: 'Bootcamp not found' });
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      );
     }
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    // res.status(400).json({ success: false });
-    next(err);
+    next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
 };
 
--- middleware/error.js
+++ middleware/error.js
@@ -1,8 +1,8 @@
 const errorHandler = (err, req, res, next) => {
   console.log(err.stack.red);
-  res.status(500).json({
+  res.status(err.statusCode || 500).json({
     success: false,
-    error: err.message,
+    error: err.message || 'Server error',
   });
 };
 module.exports = errorHandler;
