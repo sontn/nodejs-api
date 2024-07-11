d5c1973581abcc3737c194e9374daadc3ac199aa
Son Tran <son.tranngoc@gmail.com>
2024-07-11 10:18:18 +0700

    Fetching Bootcamps with GET

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,30 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc:     Get all bootcamps
 // @route:    GET /api/v1/bootcamps
 // @access:   Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc:     Get single bootcamps
 // @route:    GET /api/v1/bootcamps/:id
 // @access:   Public
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Get a bootcamp' });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+      return res
+        .status(404)
+        .json({ success: false, error: 'Bootcamp not found' });
+    }
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc:     Create new bootcamps
@@ -20,7 +35,10 @@ exports.getBootcamp = (req, res, next) => {
 exports.createBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.create(req.body);
-    res.status(201).json({ success: true, data: bootcamp });
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
   } catch (err) {
     res.status(400).json({ success: false });
   }
