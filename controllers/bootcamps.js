9f23bc923baafd1b88adee8c9176a1a2a73c61f8
Son Tran <son.tranngoc@gmail.com>
2024-07-12 11:07:27 +0700

    Adding pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -44,11 +44,41 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     query = query.sort('-createAt');
   }
 
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
   // Executing query
   const bootcamps = await query;
-  res
-    .status(200)
-    .json({ success: true, count: bootcamps.length, data: bootcamps });
+
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    pagination,
+    data: bootcamps,
+  });
 });
 
 // @desc:     Get single bootcamps
