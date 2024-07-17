c19af31fbc1bb19216ad83517bde23d0cad538ea
Son Tran <son.tranngoc@gmail.com>
2024-07-17 10:35:50 +0700

    Create user login

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Define the User schema
 const userSchema = new mongoose.Schema({
@@ -41,6 +42,18 @@ userSchema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+
+// sign JWT and return
+userSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 // Create the User model
 const User = mongoose.model('User', userSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../controllers/auth');
+const { register, login } = require('../controllers/auth');
 
 // Register a new user
 router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
