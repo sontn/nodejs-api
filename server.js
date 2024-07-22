f5a230f7059eed150f2ad8dc30a1faaf80e7ac50
Son Tran <son.tranngoc@gmail.com>
2024-07-22 13:56:37 +0700

    Rate limit, hpp and cors

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const expressRateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
 dotenv.config({ path: './config/.env' });
 
@@ -41,6 +44,20 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xss());
+
+// Rate limiting
+const limiter = expressRateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100,
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
