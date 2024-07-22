fb810799a2ce653f889416f53dc0bed93edf38f6
Son Tran <son.tranngoc@gmail.com>
2024-07-22 10:46:06 +0700

    Update and delete reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -25,6 +25,10 @@ router
   )
   .post(protect, authorize('user', 'admin'), addReview);
 
-router.route('/:id').get(getReview);
+router
+  .route('/:id')
+  .get(getReview)
+  .put(protect, authorize('user', 'admin'), updateReview)
+  .delete(protect, authorize('user', 'admin'), deleteReview);
 
 module.exports = router;
