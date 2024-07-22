d7c95ba7ab549c09bd260dc52850234bd38b503f
Son Tran <son.tranngoc@gmail.com>
2024-07-22 09:56:54 +0700

    Review model and get reviews

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -14,6 +14,7 @@ const Bootcamp = require('../models/Bootcamp');
 const advancedResults = require('../middleware/advancedResults');
 
 const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
 
 const router = express.Router();
 
@@ -21,6 +22,7 @@ const { protect, authorize } = require('../middleware/auth');
 
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadus);
 // router.route('/:bootcampId/courses').get(getCourses);
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
 const user = require('./routes/user');
+const reviews = require('./routes/reviews');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 const colors = require('colors');
@@ -36,6 +37,7 @@ app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/auth/users', user);
+app.use('/api/v1/reviews', reviews);
 
 app.use(errorHandler);
 
